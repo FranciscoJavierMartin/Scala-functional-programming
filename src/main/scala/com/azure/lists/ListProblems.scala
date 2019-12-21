992ae7fc4f3c63a08ddd69f00b90d3a00494c4c0
Francisco <javiermartincuenca@gmail.com>
2019-12-21 21:33:38 +0100

    Other list functions

--- src/main/scala/com/azure/lists/ListProblems.scala
+++ src/main/scala/com/azure/lists/ListProblems.scala
@@ -17,7 +17,16 @@ sealed abstract class RList[+T] {
 
   def reverse: RList[T]
 
-  def ++[S>:T](anotherList:RList[S]): RList[S]
+  def ++[S >: T](anotherList: RList[S]): RList[S]
+
+  def removeAt(index: Int): RList[T]
+
+  def map[S](F: T => S): RList[S]
+  def flatMap[S](f: T => RList[S]): RList[S]
+  def filter(f: T => Boolean): RList[T]
+
+  // run-length encoding
+  def rle: RList[(T, Int)]
 }
 
 case object RNil extends RList[Nothing] {
@@ -36,6 +45,16 @@ case object RNil extends RList[Nothing] {
   override def reverse: RList[Nothing] = RNil
 
   override def ++[S >: Nothing](anotherList: RList[S]): RList[S] = anotherList
+
+  override def removeAt(index: Int): RList[Nothing] = RNil
+
+  override def map[S](F: Nothing => S): RList[S] = RNil
+
+  override def flatMap[S](f: Nothing => RList[S]): RList[S] = RNil
+
+  override def filter(f: Nothing => Boolean): RList[Nothing] = RNil
+
+  override def rle: RList[(Nothing, Int)] = RNil
 }
 
 case class ::[+T](override val head: T, override val tail: RList[T]) extends RList[T] {
@@ -59,15 +78,15 @@ case class ::[+T](override val head: T, override val tail: RList[T]) extends RLi
       else applyTailrec(remaining.tail, currentIndex + 1)
     }
 
-    if(index <0) throw new NoSuchElementException
+    if (index < 0) throw new NoSuchElementException
     else applyTailrec(this, 0)
   }
 
   override def length: Int = {
     @tailrec
     def lengthTailrec(remainingList: RList[T], accumulator: Int): Int = {
-      if(remainingList.isEmpty) accumulator
-      else lengthTailrec(remainingList.tail, accumulator +1 )
+      if (remainingList.isEmpty) accumulator
+      else lengthTailrec(remainingList.tail, accumulator + 1)
     }
 
     lengthTailrec(this, 0)
@@ -75,8 +94,8 @@ case class ::[+T](override val head: T, override val tail: RList[T]) extends RLi
 
   override def reverse: RList[T] = {
     @tailrec
-    def reverseTailrec(remainingList: RList[T], accumulator:RList[T]): RList[T] ={
-      if(remainingList.isEmpty) accumulator
+    def reverseTailrec(remainingList: RList[T], accumulator: RList[T]): RList[T] = {
+      if (remainingList.isEmpty) accumulator
       else reverseTailrec(remainingList.tail, remainingList.head :: accumulator)
     }
 
@@ -86,14 +105,90 @@ case class ::[+T](override val head: T, override val tail: RList[T]) extends RLi
   override def ++[S >: T](anotherList: RList[S]): RList[S] = {
     @tailrec
     def concatTailrec(remainingList: RList[S], accumulator: RList[S]): RList[S] =
-      if(remainingList.isEmpty) accumulator
+      if (remainingList.isEmpty) accumulator
       else concatTailrec(remainingList.tail, remainingList.head :: accumulator)
 
     concatTailrec(anotherList, this.reverse).reverse
   }
+
+  override def removeAt(index: Int): RList[T] = {
+    @tailrec
+    def removeAtTailrec(remainingList: RList[T], predecessor: RList[T], currentIndex: Int): RList[T] =
+      if (currentIndex == index) predecessor.reverse ++ remainingList.tail
+      else if (remainingList.isEmpty) predecessor.reverse
+      else removeAtTailrec(remainingList.tail, remainingList.head :: predecessor, currentIndex + 1)
+
+    if (index < 0) this
+    else removeAtTailrec(this, RNil, 0)
+  }
+
+  override def map[S](F: T => S): RList[S] = {
+    @tailrec
+    def mapTailrec(remainingList: RList[T], accumulator: RList[S]): RList[S] =
+      if(remainingList.isEmpty) accumulator.reverse
+      else mapTailrec(remainingList.tail, F(remainingList.head) :: accumulator)
+
+    mapTailrec(this, RNil)
+  }
+
+  override def flatMap[S](f: T => RList[S]): RList[S] = {
+
+    /*
+      Complexity? O(Z^2)
+    */
+    @tailrec
+    def flatMapTailrec(remainingList: RList[T], accumulator: RList[S]): RList[S] = {
+      if(remainingList.isEmpty) accumulator.reverse
+      else flatMapTailrec(remainingList.tail, f(remainingList.head).reverse ++ accumulator)
+    }
+
+    /*
+      Complexity: O(N + Z)
+    */
+    @tailrec
+    def betterFlatMap(remainingList: RList[T], accumulator: RList[RList[S]]): RList[S] = {
+      if(remainingList.isEmpty) concatenateAll(accumulator, RNil, RNil)
+      else betterFlatMap(remainingList.tail, f(remainingList.head).reverse :: accumulator)
+    }
+
+    /*
+      Complexity: O(Z)
+     */
+    @tailrec
+    def concatenateAll(elements: RList[RList[S]], currentList: RList[S], accumulator: RList[S]): RList[S] = {
+      if(currentList.isEmpty && elements.isEmpty) accumulator
+      else if(currentList.isEmpty) concatenateAll(elements.tail, elements.head, accumulator)
+      else concatenateAll(elements, currentList.tail, currentList.head :: accumulator)
+    }
+
+    betterFlatMap(this, RNil)
+  }
+
+  override def filter(f: T => Boolean): RList[T] = {
+    @tailrec
+    def filterTailrec(remainingList: RList[T], accumulator: RList[T]): RList[T] =
+      if(remainingList.isEmpty) accumulator
+      else if (f(remainingList.head)) filterTailrec(remainingList.tail, head :: accumulator)
+      else filterTailrec(remainingList.tail, accumulator)
+
+
+    filterTailrec(this, RNil)
+  }
+
+  override def rle: RList[(T, Int)] = {
+    @tailrec
+    def rleTailrec(remainingList: RList[T], currentTuple: (T, Int), accumulator: RList[(T, Int)]): RList[(T, Int)] = {
+      if(remainingList.isEmpty && currentTuple._2 == 0) accumulator
+      else if (remainingList.isEmpty) currentTuple :: accumulator
+      else if (remainingList.head == currentTuple._1) rleTailrec(remainingList.tail, currentTuple.copy(_2 = currentTuple._2 + 1), accumulator)
+      else rleTailrec(remainingList.tail, (remainingList.head, 1), currentTuple :: accumulator)
+    }
+
+    rleTailrec(this.tail, (this.head, 1), RNil).reverse
+  }
 }
 
-object RList{
+object RList {
   def from[T](iterable: Iterable[T]): RList[T] = {
     @tailrec
     def convertToRListTailrec(remaining: Iterable[T], accumulator: RList[T]): RList[T] = {
@@ -108,11 +203,14 @@ object RList{
 object ListProblems extends App {
   //(2) :: RNil == RNil.::(2)
   val aSmallList = 1 :: 2 :: 3 :: RNil
+  val anotherSmallList = 1 :: 1 :: 2 :: 2 :: 3 :: RNil
   println(aSmallList)
   //println(aSmallList.apply(0))
   //println(aSmallList.apply(2))
   //println(aSmallList.apply(90))
   //println(aSmallList.length)
-  println(aSmallList.reverse)
-  println(aSmallList++aSmallList)
+  //println(aSmallList.reverse)
+  //println(aSmallList ++ aSmallList)
+  //println(aSmallList.removeAt(2))
+  println(anotherSmallList.rle)
 }
