83184b2c4e3e0b4cf9f75c8cca5bc756c67d1e8a
Francisco <javiermartincuenca@gmail.com>
2021-11-01 09:03:53 +0100

    Mirror tree

--- src/main/scala/com/azure/trees/BinaryTreeProblems.scala
+++ src/main/scala/com/azure/trees/BinaryTreeProblems.scala
@@ -4,38 +4,58 @@ import scala.annotation.tailrec
 
 sealed abstract class BTree[+T] {
   def value: T
+
   def left: BTree[T]
+
   def right: BTree[T]
+
   def isEmpty: Boolean
+
   def isLeaf: Boolean
+
   def collectLeaves: List[BTree[T]]
+
   def leafCount: Int
+
   // The number of nodes in the tree
   val size: Int
+
   def collectNodes(level: Int): List[BTree[T]]
+
   def mirror: BTree[T]
-  def sameShapeAs[S>:T](that: BTree[S]): Boolean
+
+  def sameShapeAs[S >: T](that: BTree[S]): Boolean
+
   def isSymmetrical: Boolean
 }
 
-case object BEnd extends BTree[Nothing]{
+case object BEnd extends BTree[Nothing] {
   override def value: Nothing = throw new NoSuchElementException
+
   override def left: BTree[Nothing] = throw new NoSuchElementException
+
   override def right: BTree[Nothing] = throw new NoSuchElementException
+
   override def isEmpty: Boolean = true
 
   override def isLeaf: Boolean = false
+
   override def collectLeaves: List[BTree[Nothing]] = List()
+
   override def leafCount: Int = 0
+
   override val size: Int = 0
 
   override def collectNodes(level: Int): List[BTree[Nothing]] = List()
+
   override def mirror: BTree[Nothing] = BEnd
+
   override def sameShapeAs[S >: Nothing](that: BTree[S]): Boolean = that.isEmpty
+
   override def isSymmetrical: Boolean = true
 }
 
-case class BNode[+T](override val value: T, override val left:BTree[T], override val right: BTree[T]) extends BTree[T]{
+case class BNode[+T](override val value: T, override val left: BTree[T], override val right: BTree[T]) extends BTree[T] {
   override def isEmpty: Boolean = false
 
   override def isLeaf: Boolean = left.isEmpty && right.isEmpty
@@ -43,9 +63,9 @@ case class BNode[+T](override val value: T, override val left:BTree[T], override
   override def collectLeaves: List[BTree[T]] = {
     @tailrec
     def collectLeavesTailrec(todo: List[BTree[T]], leaves: List[BTree[T]]): List[BTree[T]] = {
-      if(todo.isEmpty) leaves
-      else if(todo.head.isEmpty) collectLeavesTailrec(todo.tail, leaves)
-      else if(todo.head.isLeaf) collectLeavesTailrec(todo.tail, todo.head :: leaves)
+      if (todo.isEmpty) leaves
+      else if (todo.head.isEmpty) collectLeavesTailrec(todo.tail, leaves)
+      else if (todo.head.isLeaf) collectLeavesTailrec(todo.tail, todo.head :: leaves)
       else {
         val node = todo.head
         collectLeavesTailrec(node.left :: node.right :: todo.tail, leaves)
@@ -61,8 +81,8 @@ case class BNode[+T](override val value: T, override val left:BTree[T], override
 
   override def collectNodes(level: Int): List[BTree[T]] = {
     @tailrec
-    def collectNodesTailrec(currentLevel:  Int, currentNodes: List[BTree[T]]): List[BTree[T]] = {
-      if(currentNodes.isEmpty)List()
+    def collectNodesTailrec(currentLevel: Int, currentNodes: List[BTree[T]]): List[BTree[T]] = {
+      if (currentNodes.isEmpty) List()
       else if (currentLevel == level) currentNodes
       else {
         val expandedNodes = for {
@@ -74,11 +94,31 @@ case class BNode[+T](override val value: T, override val left:BTree[T], override
       }
     }
 
-    if(level < 0) List()
+    if (level < 0) List()
     else collectNodesTailrec(0, List(this))
   }
 
-  override def mirror: BTree[T] = ???
+  override def mirror: BTree[T] = {
+    @tailrec
+    def mirrorTailrec(todo: List[BTree[T]], expanded: Set[BTree[T]], done: List[BTree[T]]): BTree[T] = {
+      if (todo.isEmpty) done.head
+      else {
+        val node = todo.head
+        if (node.isEmpty || node.isLeaf) {
+          mirrorTailrec(todo.tail, expanded, node :: done)
+        } else if (!expanded.contains(node)) {
+          mirrorTailrec(node.left :: node.right :: todo, expanded + node, done)
+        } else {
+          val newLeft = done.head
+          val newRight = done.tail.head
+          val newNode = BNode(node.value, newLeft, newRight)
+          mirrorTailrec(todo.tail, expanded, newNode :: done.drop(2))
+        }
+      }
+    }
+
+    mirrorTailrec(List(this), Set(), List())
+  }
 
   override def sameShapeAs[S >: T](that: BTree[S]): Boolean = ???
 
@@ -124,4 +164,6 @@ object BinaryTreeProblems extends App {
   println(tree.collectNodes(0).map(_.value))
   println(tree.collectNodes(2).map(_.value))
   println(tree.collectNodes(6473).map(_.value))
+
+  println(tree.mirror)
 }
